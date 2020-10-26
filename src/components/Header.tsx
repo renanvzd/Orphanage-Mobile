1cf5519aba87cc446605b0b126c576ccbbf6a274
Renan Drechsler <renanvz@outlook.com>
2020-10-26 19:52:34 -0300

    Header - Arrow Left

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import { BorderlessButton } from 'react-native-gesture-handler';
+import { Feather } from '@expo/vector-icons'
 
 interface HeaderProps {
     title: string;
@@ -8,6 +10,9 @@ interface HeaderProps {
 export default function Header({ title }: HeaderProps) {
     return (
     <View style={styles.container}>
+        <BorderlessButton onPress={() => {}}>
+            <Feather name="arrow-left" size={24} color="#15b6d6" />
+        </BorderlessButton>
         <Text style={styles.title}>{title}</Text>
     </View>
     );
